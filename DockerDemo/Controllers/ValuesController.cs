474e0b7ce1f1820a9890ce872aac02ac00fa3eaf
xiaowei <xiaowei5966@outlook.com>
2021-05-04 22:26:20 +0800

    test

--- DockerDemo/Controllers/ValuesController.cs
+++ DockerDemo/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace DockerDemo.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value11", "value22" };
         }
 
         // GET api/<ValuesController>/5
